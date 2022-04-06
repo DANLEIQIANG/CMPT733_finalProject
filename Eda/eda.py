14f2b6b19cbaeff277590b200b9ec391ef45ff38
TripleW-SFU <43801413+t2wan@users.noreply.github.com>
2022-04-06 00:27:46 -0700

    eda & classification

--- Eda/eda.py
+++ Eda/eda.py
@@ -7,6 +7,8 @@ import seaborn as sns
 import nltk
 import re
 from nltk.tokenize import word_tokenize
+import plotly.express as px
+import geopandas as gpd
 
 def main(dataset):
     #setup environment if necessary
@@ -22,13 +24,11 @@ def main(dataset):
     
     #plot countries
     df = df[df.geo_country_code.isin(['NZ','AU','CA','GB','US'])]
-    df['geo_country_code'].value_counts().plot(kind="barh")
-    plt.title('Country Distribution of Tweets')
-    plt.xlabel('Country')
-    plt.ylabel('Count')
-    plt.gca().invert_yaxis()
-    plt.savefig(outpath + 'country_distribution.png')
-    plt.close()
+    data = pd.DataFrame(df['geo_country_code'].value_counts())
+    data['country'] = data.index
+    fig = px.bar(data, x='country', y='geo_country_code',
+                 title='Country Distribution of Tweets')
+    fig.write_image(outpath + 'country_distribution.png')
 
     #token distribution
     clean = '@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+'
@@ -45,48 +45,29 @@ def main(dataset):
     token = pd.DataFrame.from_dict(tokens,orient='index', columns = ['count'])
     token.reset_index(inplace=True)
     token.columns = ['term','count']
-    plt.figure()
-    plt.scatter(x = list(token[:20]['count']),
-                            y =list(token[:20]['term']),
-                            linewidths = 2,
-                            edgecolor='b',
-                            alpha = 0.5)
-    plt.title('Tokenization Top20 Words Frequency')
-    plt.xlabel('Frequency')
-    plt.ylabel('Words')
-    plt.gca().invert_yaxis()
-    plt.savefig(outpath + 'token_distribution.png')
-    plt.close()
+    fig = px.scatter(token.head(20), x="count", y="term", title='Tokenization Top20 Words Frequency')
+    fig.write_image(outpath + 'token_distribution.png')
 
     #word count
     df['text_length'] = df['text'].apply(lambda x:len(x))
-    plt.figure()
-    sns.boxplot(x=df['text_length']).set_title('Word count in Each Tweet')
-    plt.title('Word count in Each Tweet')
-    plt.xlabel('Length')
-    plt.savefig(outpath + 'box_plot_tweet_length.png')
-    plt.close()
+    fig = px.box(df, y="text_length")
+    fig.write_image(outpath + 'box_plot_tweet_length.png.png')
 
     #user_created_time
     df['user_created_time'] = df['user_created_time'].apply(lambda x:datetime.strptime(x[:10], '%Y-%m-%d'))
-    plt.figure()
-    plt.hist(x=df['user_created_time'])
-    plt.title('user_created_time')
-    plt.xlabel('Year')
-    plt.ylabel('Frequency')
-    plt.savefig(outpath + 'user_created.png')
-    plt.close()
+    fig = px.histogram(df, x="user_created_time", title='user_created_time')
+    fig.write_image(outpath + 'user_created.png')
+    
 
     #account history by country
     df['account_history'] = df['user_created_time'].apply(lambda x:(datetime.now()-x).days/365)
     aggd = df.groupby('geo_country_code').mean()
-    plt.figure()
-    plt.bar(aggd.index.tolist(),aggd['account_history'])
-    plt.title('average account history by country')
-    plt.xlabel('Country')
-    plt.ylabel('Years')
-    plt.savefig(outpath + 'history_by_country.png')
-    plt.close()
+    aggd['country'] = aggd.index
+    fig = px.bar(aggd, x='country', y='account_history',
+                 title='Country Distribution of Tweets')
+    fig.write_image(outpath + 'history_by_country.png')
+    #plt.savefig(outpath + 'history_by_country.png')
+    #plt.close()
 
 
 if __name__ == '__main__':
